7958af2d77268e5c6eafd38e64479d051e4fd42c
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-09-08 15:13:54 +0300

    Add-files

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import s from './App.module.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className={s.Container}>Hello</div>
+    );
+  }
+}
